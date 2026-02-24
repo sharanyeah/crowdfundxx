ac4760d6538699c7bd7d072f146de8edc5874390
sharanyeah <kallemsharanyareddy21@gmail.com>
2026-02-24 13:49:39 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -788,6 +788,6 @@ const root = process.cwd();
 
 app.use(express.static(path.join(root, "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(root, "dist", "index.html"));
 });
